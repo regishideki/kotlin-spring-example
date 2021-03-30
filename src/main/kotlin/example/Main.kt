fe012393631cfd61ddbb928b0fcfa7f5483f01a6
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2021-03-30 09:19:02 -0300

    add simple controller

---
+++ src/main/kotlin/example/Main.kt
@@ -0,0 +1,12 @@
+package example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+open class Main
+
+fun main() {
+    runApplication<Main>()
+}
---
+++ src/test/kotlin/example/web/HealthControllerTest.kt
@@ -0,0 +1,23 @@
+package example.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `it returns OK`() {
+        val result = mockMvc.perform(get("/health"))
+
+        result.andExpect(status().isOk)
+    }
+}
