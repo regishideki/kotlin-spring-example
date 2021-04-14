5698b508bdc12bd8ec7d778bc7052224b634cbce
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2021-04-14 09:35:07 -0300

    Fixing change in build.gradle to build.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+
+        maven {
+            url = java.net.URI("https://plugins.gradle.org/m2/")
+        }
+    }
+}
+rootProject.name = "kotlin-spring-example"
+
